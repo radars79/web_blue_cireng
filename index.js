72a70162ec2cb4e7f27224167e0e443b7bbbe82c
radars79 <151916901+radars79@users.noreply.github.com>
2025-06-03 07:02:34 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// toggle class active
+const navbarnav= document.querySelector ('.navbar-nav');
+// onclick menu
+document.querySelector ('#menu')
+.onclick=()=> {
+    navbarnav.classList.toggle('active')
+} ;
